604af642e8a7d529f0eaa96431cccd8b702d53da
JulenMendiguren <mendihyk@hotmail.com>
2019-04-05 20:11:20 +0200

    yo ya no se

--- service-worker.js
+++ service-worker.js
@@ -43,7 +43,7 @@ this.addEventListener('fetch', event => {
         // Respond with everything else if we can
         event.respondWith(caches.match(event.request)
             .then(function (response) {
-                return response //|| fetch(event.request)
+                return response || fetch(event.request)
             })
         );
     }
